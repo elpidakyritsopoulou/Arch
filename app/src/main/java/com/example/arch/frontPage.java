9f5934ef1b775a31e02b0ba9b892acda8b3c84fd
elpidakyritsopoulou <elpidakiri@gmail.com>
2023-08-07 18:25:21 +0300

    frontPage

--- app/src/main/java/com/example/arch/frontPage.java
+++ app/src/main/java/com/example/arch/frontPage.java
@@ -2,20 +2,37 @@ package com.example.arch;
 
 
 
-import static androidx.core.content.ContextCompat.startActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class frontPage {
 
-//    public void goTofirst_page (View view){
-//        Intent intent = new Intent (this, first_page.class);
-//        startActivity(intent);
+public class frontPage extends AppCompatActivity {
+
+    private Button welcome_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+
+        welcome_button = (Button) findViewById(R.id.welcome_button);
+        welcome_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openfirstpage();
+            }
+        });
     }
 
-//    public void goTofirst_page(View view) {
-//    }
-//}
+    public void openfirstpage() {
+        Intent intent = new Intent(this,first_page.class);
+        startActivity(intent);
+    }
+
+}
+
+
 
