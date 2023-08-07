c5aec042f254489df4929f535ab703be422fbc1c
elpidakyritsopoulou <elpidakiri@gmail.com>
2023-08-07 15:51:15 +0300

    front_page

---
+++ app/src/main/java/com/example/arch/first_page.java
@@ -0,0 +1,8 @@
+package com.example.arch;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class first_page {
+}
--- app/src/main/java/com/example/arch/frontPage.java
+++ app/src/main/java/com/example/arch/frontPage.java
@@ -1,4 +1,19 @@
 package com.example.arch;
 
+
+
+import static androidx.core.content.ContextCompat.startActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
 public class frontPage {
+
+    public void goTofirst_page (View view){
+        Intent intent = new Intent (this, first_page.class);
+        startActivity(intent);
+    }
+
 }
+
