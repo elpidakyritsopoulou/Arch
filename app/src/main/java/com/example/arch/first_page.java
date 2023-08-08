30aacd17c90ea4974a70851226b0b9bf4c1ee076
PETROSDIAMAN99 <petrosdiaman99@gmail.com>
2023-08-08 20:43:19 +0300

    patras

--- app/src/main/java/com/example/arch/first_page.java
+++ app/src/main/java/com/example/arch/first_page.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class first_page extends AppCompatActivity {
 
+    public first_page(patras patras, Class<first_page> firstPageClass) {
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_page);
--- app/src/main/java/com/example/arch/patras.java
+++ app/src/main/java/com/example/arch/patras.java
@@ -2,6 +2,7 @@ package com.example.arch;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,11 @@ public class patras extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patras);
+    }
+    public void startSecondActivity(frontPage view) {
+        frontPage intent = new frontPage ();
+        Intent frontPage = null;
+        startActivity(frontPage);
     }
 
 }
