af4e2adde49cec977c76fd7cb6cd3b084fb07067
elpidakyritsopoulou <elpidakiri@gmail.com>
2023-09-25 22:36:06 +0200

    front page changes

---
+++ app/src/main/java/com/example/arch/Odeon_of_Herodus_Atticus.java
@@ -0,0 +1,63 @@
+package com.example.arch;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Odeon_of_Herodus_Atticus extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.odeon_of_herodus_atticus);
+
+
+//        ImageButton link1 = (ImageButton) findViewById(R.id.link1);
+//        ImageButton link2 = (ImageButton) findViewById(R.id.link2);
+//        ImageButton link3 = (ImageButton) findViewById(R.id.link3);
+//
+//
+//        link1.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View view) {
+//                goLink("https://www.visitgreece.gr/experiences/culture/archaeological-sites-and-monuments/acropolis-of-athens/");
+//
+//            }
+//
+//        });
+//
+//        link2.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View view) {
+//                goLink("https://whc.unesco.org/en/list/404/");
+//
+//            }
+//
+//        });
+//
+//        link3.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View view) {
+//                goLink("https://www.theacropolismuseum.gr/en/");
+//
+//            }
+//
+//        });
+//    }
+//    private void goLink(String s) {
+//        Uri uri = Uri.parse(s);
+//        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+//    };
+
+
+    }
+}
+
+
+
+
--- app/src/main/java/com/example/arch/athens_monuments.java
+++ app/src/main/java/com/example/arch/athens_monuments.java
@@ -20,6 +20,8 @@ public class athens_monuments extends AppCompatActivity {
         setContentView(R.layout.athens_monuments);
 
         ImageView acropolis = (ImageView) findViewById(R.id.acropolis);
+        ImageView panathenaic_stadium = (ImageView) findViewById(R.id.panathenaic_stadium1);
+        ImageView herodus_atticus = (ImageView) findViewById(R.id.herodusatticus);
 
         acropolis.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +29,19 @@ public class athens_monuments extends AppCompatActivity {
                 startActivity(new Intent(athens_monuments.this, acropolis.class));
             }
         });
+
+        panathenaic_stadium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(athens_monuments.this, Panathenaic_stadium.class));
+            }
+        });
+
+        herodus_atticus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(athens_monuments.this, Odeon_of_Herodus_Atticus.class));
+            }
+        });
     }
-
-
-
-
 }
