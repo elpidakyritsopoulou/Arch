17312ae8b6fde8e8d2efb5411cbb9a9b6fab8937
elpidakyritsopoulou <elpidakiri@gmail.com>
2023-10-01 17:04:31 +0200

    Italy images

---
+++ app/src/main/java/com/example/arch/maratea.java
@@ -0,0 +1,36 @@
+package com.example.arch;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class maratea extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maratea);
+
+        Button backbutton = (Button) findViewById(R.id.backbutton);
+        Button readmore = (Button) findViewById(R.id.button);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(maratea.this, Italy.class));
+            }
+        });
+
+        readmore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(maratea.this, athens_monuments.class));
+            }
+        });
+    }
+
+
+}
