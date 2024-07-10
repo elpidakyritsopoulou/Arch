168e01c67fa33542086ec80a314f33c330ed5ac6
elpid <elpidakiri@gmail.com>
2024-07-10 18:31:44 +0200

    09.07.2024

--- app/src/main/java/com/example/arch/albacin.java
+++ app/src/main/java/com/example/arch/albacin.java
@@ -1,6 +1,7 @@
 package com.example.arch;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AlertDialog;
@@ -130,8 +132,15 @@ public class albacin extends AppCompatActivity {
 
 
         ImageButton link1 = (ImageButton) findViewById(R.id.link1);
+         Button location_albacin = (Button) findViewById(R.id.location_albacin);
 
-
+        String locationUrl = "https://maps.app.goo.gl/4xFwN3zk4vz7b6LS9";
+        location_albacin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(locationUrl)));
+            }
+        });
 
         link1.setOnClickListener(new View.OnClickListener() {
 
